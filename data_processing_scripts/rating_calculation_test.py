208a92fe03db55199c6ce5804fedbb90d88fc141
sangjunp4rk <sp3394@columbia.edu>
2019-04-27 16:51:30 -0400

    python data processing done

---
+++ data_processing_scripts/rating_calculation_test.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+import numpy as np
+
+
+df = pd.read_csv('games_data.csv', sep=',')
+
+## CALCULATE CATEGORY RATING ##
+category_name = 'Economic'
+
+# filter based on category
+filtered_categories_df = df[df['categories'].str.contains(category_name) == True]
+
+# filter 0s
+filtered_categories_df = filtered_categories_df[filtered_categories_df['average_rating'] != 0.0]
+
+category_mean = filtered_categories_df['average_rating'].mean()
+
+################
+
+## CALCULATE MECHANICS ##
+mechanics_name = 'Dice Rolling'
+
+# filter based on mechanics_name
+filtered_mechanics_df = df[df['mechanics'].str.contains(mechanics_name) == True]
+
+# filter 0s
+filtered_mechanics_df = filtered_mechanics_df[filtered_mechanics_df['average_rating'] != 0.0]
+
+mechanics_mean = filtered_mechanics_df['average_rating'].mean()
+
+################
+
+## CALCULATE MIN PLAYERS ##
+min_players = 2
+
+# filter based on min players
+filtered_min_players_df = df[df['minplayers'] == min_players]
+
+# filter 0s
+filtered_min_players_df = filtered_min_players_df[filtered_min_players_df['average_rating'] != 0.0]
+
+min_players_mean = filtered_min_players_df['average_rating'].mean()
+
+################
+
+## CALCULATE AVERAGE MEAN ##
+average_list = [category_mean, mechanics_mean, min_players_mean]
+estimated_average = reduce(lambda x, y: x + y, average_list) / len(average_list)
+
+print estimated_average
--- server.py
+++ server.py
@@ -7,10 +7,17 @@ import csv
 
 app = Flask(__name__)
 
+game_data = []
+
 ######## Routes for Main Page ########
 @app.route('/')
 def main():
 	return render_template('main.html', page_name='main')
+
+@app.route('/save_expenses', methods=['GET', 'POST'])
+def load_data('/load_data'):
+	print 'test'
+
 
 if __name__ == '__main__':
    app.run(debug = True)
